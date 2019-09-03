d1522ef91638fe54796f8afc1453a51399c9aa85
Damirhub <damir.pjetlovic@gmail.com>
2019-09-03 17:15:08 +0200

    firstOnes from variable to useState

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,28 +3,35 @@ import React, { useState, useEffect } from 'react'
 
 const Grid = ({ next, setStarting }) => {
 
-    const firstOnes = next.map(a => a[0])
+    // const firstOnes = next.map(a => a[0])
 
-    // const bla = firstOnes[0]
+    useEffect(() => {
+        setFirstOnes(next.map(a => a[0]))
+
+    }, [next])
+
+    const [firstOnes, setFirstOnes] = useState([])
+
+
+    console.log('FIRST ONES USE TO ENABLE NEXT', firstOnes);
 
     const [clicked, setClicked] = useState()
-    const [allowedNext, setAllowedNext] = useState([15, 33])
+    console.log('1. CLICKED', clicked);
+    const [allowedNext, setAllowedNext] = useState([15, 12, 33, 45])
 
 
     // useEffect(() => {
     //     setClicked(firstOnes[0])
     // })
 
+    // let eliminateClicked = () => (firstOnes.map( (first, i) => first[i] === clicked) ) &&  firstOnes.shift() 
+
+    // let eliminateClicked = () => (firstOnes.map( (first, i) => first[i] === clicked) ) &&  firstOnes.shift() 
+    // console.log("FIRSTONES", eliminateClicked())
 
     const grid = 100
-    // console.log("NEXTTTTT", next.flat())
-    // const flat = next.flat()
 
-    // let unique = [...new Set(flat)]
-
-
-
-    /********************************************** */
+    /************************************************** */
     /************************************************** */
 
 
@@ -32,11 +39,19 @@ const Grid = ({ next, setStarting }) => {
         // possible dirrection
         const nextDirrection = next.map(x => x.filter(ele => [clicked].includes(ele)))
 
-    // finding indexes with excluded clicked value
+        console.log("1. NEXT DIRRECTION", nextDirrection)
+
+        // const isUsed = (firstOnes.map( (first, i) => first[i] === clicked) ) &&  firstOnes.splice(i) 
+        // console.log('isUsed', isUsed);
+
+        // finding indexes with excluded clicked value
         const allowedDirrections = nextDirrection.reduce(
             (arr, e, i) => (e.includes(clicked) && firstOnes[i] !== clicked && arr.push(i), arr), []
         )
 
+        console.log("2. ALLOWED DIRRECTIONS INDEXES", allowedDirrections)
+
+        // REMOVED THE PREVIOUS ONE
         return firstOnes.map((i, j) => firstOnes[allowedDirrections[j]]).filter(Number)
 
     }
@@ -52,7 +67,7 @@ const Grid = ({ next, setStarting }) => {
 
     // console.log("clicked",clicked)
 
-    console.log('FIRST ONES USE TO ENABLE NEXT', firstOnes);
+
     console.log("ALLOWED NEXT FIELDS", allowedNext)
 
 
@@ -68,14 +83,17 @@ const Grid = ({ next, setStarting }) => {
 
                             {
                                 (firstOnes.map((z, y) => {
-                                    return <>
+                                    return <div key={y}>
                                         {(z === i + 1) &&
                                             <button key={i}
-                                                // disabled={z !== allowedNext[0] && true && z !== allowedNext[1] && true}
+                                                disabled={
+                                                    z !== allowedNext[0] && allowedNext[0] && true && z !== allowedNext[1] && true && z !== allowedNext[2] && true
+                                                    && z !== allowedNext[3] && true && z !== allowedNext[4] && true && z !== allowedNext[5] && true && z !== allowedNext[6] && true
+                                                }
 
-                                                disabled={ ( z !== allowedNext[y])  && true }
+                                                // disabled={   allowedNext[y] === undefined ? false : z !==  allowedNext[y]   && true }
                                                 onClick={() => {
-                                                    clickedValue(z);
+                                                    clickedValue(z, i);
                                                 }
                                                 }
 
@@ -83,24 +101,23 @@ const Grid = ({ next, setStarting }) => {
                                                 {/* { console.log("allowedNextUUUUUUUUUUUUUUUUU",  allowedNext[y] !== undefined ? allowedNext[y] : true   ) } */}
 
                                                 {
-                                                    console.log("ZZZZZZZZZZZZZZ", z ),
-                                                    console.log("allowedNext", z === allowedNext[y] && z + 'x')
-                    
-                                            }
+                                                    // console.log("ZZZZZZZZZZZZZZ", z ),
+                                                    // console.log("ALLLLOOOOOOOWEEEEED", allowedNext[y] )
+                                                    // console.log("allowedNext", 
+                                                    // z === allowedNext[y] && z 
+
+
+                                                    // )
+
+                                                }
                                                 {z}
 
-                                            </button>}
-                                        {
-                                            // z === allowedFieldsNum[0] &&
-                                            // <div key={i} onClick={() => clickedValue(z)} className="possible2">
-                                            //     {z}
-                                            // </div>
+                                            </button>
                                         }
-                                    </>
+                                    </div>
 
                                 }))
                             }
-
 
                         </div>
                     )
